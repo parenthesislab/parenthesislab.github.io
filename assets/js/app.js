ed2bfa06efbefa358ef4db1ae93f88c43b949ab8
elvismdev <elvismdev@gmail.com>
2016-02-29 01:48:56 -0500

    Working code in react to switch content in center of the page when clicked on the menu items.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,116 @@
-var HelloWorld = React.createClass({
-  render: function() {
+var FooterMenu = React.createClass({
+  onClick: function (type) {
+    var content;
+
+    switch (type) {
+      case 'about':
+      content = <p>Parenthesis Tech is a leading software development company providing technology
+                solutions for organizations and individuals across the globe. We provide a
+                broad scope of services in areas of Web Development, Hybrid Mobile Apps, Desktop
+                Software and Server/Cloud-Based Applications & Management. The Parenthesis team
+                prides itself on having unparalleled expert programmers ready to apply their
+                knowledge and skill to bring you success. Quality, communication and transparency
+                are our goals to always deliver the best results.<br />
+                We don't want it perfect, we want it awesome.</p>;
+      break;
+      case 'tech-stack':
+      content = <p>PHP - HTML5 - CSS3 - JavaScript - jQuery - AngularJS - Symfony - ReactJS -
+                Laravel - Linux - NodeJS - Drupal - WordPress - MySQL - APIs - Ionic - PhoneGap -
+                MongoDB - Amazon AWS - Java</p>;
+      break;
+      default:
+      throw new Error('Unimplemented type');
+    }
+
+    this.props.setActiveContent(content);
+  },
+  render: function () {
     return (
-      <h1>(parenthesis)</h1>
+      <footer className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <ul className="text-muted list-inline main-menu">
+                <li onClick={this.onClick.bind(this, 'about')}>(about)</li>
+                <li onClick={this.onClick.bind(this, 'tech-stack')}>(tech stack)</li>
+              </ul>
+            </div>
+            <div className="col-md-6">
+              <ul className="text-muted list-inline social-links">
+                <li>
+                <a href="mailto:hi@parenthesis.io"><span className="typcn typcn-social-at-circular"></span></a>
+                </li>
+                <li>
+                <a href="https://www.linkedin.com/company/parenthesis-tech"><span className="typcn typcn-social-linkedin-circular"></span></a>
+                </li>
+                <li>
+                <a href="https://twitter.com/ParenthesisTech"><span className="typcn typcn-social-twitter-circular"></span></a>
+                </li>
+                <li>
+                <a href="https://github.com/parenthesislab"><span className="typcn typcn-social-github-circular"></span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
       );
   }
 });
 
+var MainContent = React.createClass({
+  contextTypes: {
+    activeContent: React.PropTypes.any
+  },
+  render: function () {
+    return (
+      <div className="container">
+        <div className="row">{this.context.activeContent}</div>
+      </div>
+      );
+  }
+});
+
+var MainContentWrapper = React.createClass({
+  render: function () {
+    return <MainContent />;
+  }
+});
+
+var App = React.createClass({
+
+  getInitialState: function () {
+    return {
+      activeContent: <h1>(parenthesis)</h1>
+    }
+  },
+
+  childContextTypes: {
+    activeContent: React.PropTypes.any
+  },
+
+  getChildContext: function () {
+    return {
+      activeContent: this.state.activeContent
+    };
+  },
+
+  setActiveContent: function (content) {
+    this.setState({activeContent: content});
+  },
+
+  render: function () {
+    return (
+      <div>
+        <MainContentWrapper />
+        <FooterMenu setActiveContent={this.setActiveContent} />
+      </div>
+      );
+  }
+
+});
+
 ReactDOM.render(
-  <HelloWorld/>,
-  document.getElementById('content-wrapper')
+  <App />,
+  document.getElementById('content')
   );
